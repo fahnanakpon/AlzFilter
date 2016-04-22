ef49804ac5cd13637976fe7194bf0df3b53f9310
Anakpon <Prathanrach>
2016-04-22 09:10:51 +0700

    filter app

--- AlzFilter/ResultViewController.swift
+++ AlzFilter/ResultViewController.swift
@@ -1,14 +1,8 @@
-//
-//  ResultViewController.swift
-//  AlzFilter
-//
-//  Created by Fahn on 4/21/16.
-//  Copyright © 2016 Fahn. All rights reserved.
-//
-
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    var toPass:String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +14,9 @@ class ResultViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
+    @IBOutlet weak var score: UILabel!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
